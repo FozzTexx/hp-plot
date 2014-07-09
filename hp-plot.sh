d9eda9feb304d722a7ef69c91044a1cf3e69d0e3
Chris Osborn <fozztexx@fozztexx.com>
2014-07-09 10:48:13 -0700

    Added GPL license.

--- hp-plot.sh
+++ hp-plot.sh
@@ -1,6 +1,22 @@
 #!/bin/sh
 # Quick little script that will convert an eps file into hpgl
 # Copyright 1998-2014 Chris Osborn <fozztexx@fozztexx.com> 
+# This file is part of hp-plot.
+#
+# hp-plot is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+#
+# hp-plot is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with hp-plot; see the file COPYING. If not see
+# <http://www.gnu.org/licenses/>.
+
 PATH=/usr/local/bin:/usr/bin:/bin
 export PATH
 TMP=/tmp/prout.$$
